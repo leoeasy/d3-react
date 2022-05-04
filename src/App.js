58795b859299012562e33cf6124c912c1f51855b
Abbas Moghadasi <abm@schaeffer-ag.de>
2022-05-04 13:40:37 +0200

    we have d3 added and join methods

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
 import { useEffect, useRef } from "react";
+import { select } from "d3";
+const data = [10, 20, 50, 70];
 function App() {
   const svgRef = useRef();
 
   useEffect(() => {
     console.log("svg ref", svgRef);
+    const svg = select(svgRef.current);
+    svg
+      .selectAll("circle")
+      .data(data)
+      .join(
+        (enter) => enter.append("circle"),
+        (update) => update.attr("class", "updated"),
+        (exit) => exit.remove()
+      );
   }, []);
   return <svg ref={svgRef}></svg>;
 }
